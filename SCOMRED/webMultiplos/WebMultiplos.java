74bd57eac7a96f5a020c7d0320e7037276009ffe
antoniocanelas <antoniocanelas@gmail.com>
2017-11-07 22:08:15 +0000

    webMultiplos update

--- SCOMRED/webMultiplos/WebMultiplos.java
+++ SCOMRED/webMultiplos/WebMultiplos.java
@@ -6,11 +6,24 @@ import java.net.Socket;
 
 public class WebMultiplos {
 
+    private static int contador = 0;
+
+    public static synchronized int getContador() {
+
+	return contador;
+    }
+
+    public static synchronized void incContador() {
+
+	contador++;
+    }
+
     public static void main(String[] args) {
 
 	ServerSocket ss = null;
 	try {
 	    ss = new ServerSocket(8080);
+
 	} catch (IOException ioe) {
 	    System.out.println("Erro ao abrir o ServerSocket:" + ioe.getMessage());
 	    System.exit(1);
@@ -20,6 +33,7 @@ public class WebMultiplos {
 	while (true) {
 	    try {
 		Socket cs = ss.accept();
+		incContador();
 		Thread th = new Thread(new HTTPclient(cs));
 		th.start();
 	    } catch (IOException ioe) {
